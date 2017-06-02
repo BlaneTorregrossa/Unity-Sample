b1ffe7456cd0102c827864ea57ab363e378c75e2
BlaneTorregrossa <blanet97@cox.net>
2017-06-02 15:54:28 -0500

    Sample Commit Summary

---
+++ Assets/Sample Folder/SampleScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SampleScript : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
